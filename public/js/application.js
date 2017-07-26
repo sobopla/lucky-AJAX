8c1c696bf3914773dfb8b7bc1dcc47cb2ee7d6ad
Catherine Jean <catherinejeanbean@gmail.com>
2017-07-26 00:34:04 -0500

    Set up ajax post, update span class w ajax response

--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,21 @@
 $(document).ready(function () {
 	$("#die_action").submit(function(event){
 		event.preventDefault();
+
+		// do an ajax request to /rolls:
+		var form = $(this);
+		var form_data = form.serialize();
+		$.ajax({
+			url: form.attr('action'), 
+			//or hardcode with /rolls
+			type: form.attr('method'),
+			data: form_data
+		})
+
+		//update span class with the ajax response
+		.done(function(dataReturn) {
+			$("div#die-container").html('<div class="die"><span class="roll">' + dataReturn + "</span></roll>");
+		})
 	})
 	
 
